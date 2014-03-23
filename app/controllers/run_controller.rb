2c68f362042ce8af48cad5041e1388ce37a68b61
Dean Perry <dean@voupe.com>
2014-03-23 15:34:14 +0000

    redirect to the free agent account edit page if the main & stripe accounts aren't set

--- app/controllers/run_controller.rb
+++ app/controllers/run_controller.rb
@@ -4,9 +4,13 @@ class RunController < ApplicationController
 
 	def go
 		if current_user.freeagent_account.present? && current_user.stripe_account.present?
-			log = current_user.logs.create(content: "")
-			Resque.enqueue(Run, current_user.id, log.id)
-			redirect_to log_path(log), notice: "Import started. Below is the log for it."
+			if current_user.freeagent_account.main.present? && current_user.freeagent_account.stripe.present?
+				log = current_user.logs.create(content: "")
+				Resque.enqueue(Run, current_user.id, log.id)
+				redirect_to log_path(log), notice: "Import started. Below is the log for it."
+			else
+				redirect_to edit_freeagent_account_path(current_user.freeagent_account), alert: "You need to set your Main & Stripe account IDs"
+			end
 		else
 			redirect_to root_path, alert: "You don't have a FreeAgent or Stripe account linked"
 		end		
