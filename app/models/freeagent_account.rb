a9e7d76aa4bae64662f0119f3377537cc56b4292
Dean Perry <dean.perry@outlook.com>
2014-10-03 21:32:40 +0100

    added code to refresh a freeagent token if it's expired

--- app/models/freeagent_account.rb
+++ app/models/freeagent_account.rb
@@ -14,4 +14,25 @@ class FreeagentAccount < ActiveRecord::Base
     end
   end
 
+  def refresh_token
+    response    = RestClient.post "https://api.freeagent.com/v2/token_endpoint", :grant_type => 'refresh_token', :refresh_token => self.refresh_token, :client_id => ENV["FREEAGENT_ID"], :client_secret => ENV["FREEAGENT_SECRET"] 
+    refreshhash = JSON.parse(response.body)
+
+    token_will_change!
+    expires_at_will_change!
+
+    self.token     = refreshhash['access_token']
+    self.expires_at = DateTime.now + refreshhash["expires_in"].to_i.seconds
+
+    self.save
+  end
+
+  def token_expired?
+    expiry = Time.at(self.expires_at) 
+    return true if expiry < Time.now # expired token, so we should quickly return
+    token_expires_at = expiry
+    save if changed?
+    false # token not expired. :D
+  end
+
 end
